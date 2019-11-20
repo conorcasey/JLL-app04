ca55b5a53c91e80d4263619f6482b0043269d20a
Conor Casey <conor.casey@am.jll.com>
2019-11-20 15:27:10 -0600

    updates for pipeline run status checking inside TFE

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -123,7 +123,7 @@ resource "panos_security_rule_group" "example" {
     action                = var.action
   }
   rule {
-    name                  = "Allow : 10.30.68.20 to 10.30.80.20"
+    name                  = "Allow - 10.30.68.20 to 10.30.80.20"
     source_zones          = ["DMZ"]
     source_addresses      = ["10.30.68.20"]
     source_users          = var.source_users
